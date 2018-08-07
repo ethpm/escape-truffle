456da1f50aaa411f08a56542f131fdd8e89c0cc5
cgewecke <christophergewecke@gmail.com>
2018-08-06 19:50:48 -0700

    Add tests for getAllPackageIds

--- test/packageDB.js
+++ test/packageDB.js
@@ -133,4 +133,145 @@ contract('PackageDB', function(accounts){
       );
     });
   });
+
+  describe('getAllPackageIds', function(){
+    let packageNames = ['a','b','c','d','e'];
+    let packageHashes = [];
+
+    before(async ()=> {
+      for (let name of packageNames){
+        let hash = await packageDB.hashName(name);
+        packageHashes.push(hash);
+      }
+    })
+
+    beforeEach(async () => packageDB = await PackageDB.new());
+
+    it('returns ([],0) for an empty DB', async() => {
+      const result = await packageDB.getAllPackageIds(0,20);
+
+      assert(Array.isArray(result.packageIds));
+      assert(result.packageIds.length === 0);
+      assert(result.offset.toNumber() === 0)
+    });
+
+    it('returns ([],0) when limit param is zero', async()=> {
+      const result = await packageDB.getAllPackageIds(0,0);
+
+      assert(Array.isArray(result.packageIds));
+      assert(result.packageIds.length === 0);
+      assert(result.offset.toNumber() === 0)
+    });
+
+    it('returns ([],offset) when offset equals DB size', async()=>{
+      const dbSize = await packageDB.getNumPackages();
+      const offset = dbSize.toNumber();
+      const result = await packageDB.getAllPackageIds(offset,20);
+
+      assert(Array.isArray(result.packageIds));
+      assert(result.packageIds.length === 0);
+      assert(result.offset.toNumber() === offset)
+    });
+
+    it('returns ([allPackageIds], limit) when limit is greater than DB size', async()=>{
+      for (let name of packageNames){
+        await packageDB.setPackage(name);
+      }
+
+      const dbSize = await packageDB.getNumPackages();
+
+      assert(dbSize.toNumber() === packageNames.length);
+      const limit = dbSize.toNumber() + 1;
+      const result = await packageDB.getAllPackageIds(0,limit);
+
+      assert(result.packageIds.length === dbSize.toNumber());
+      assert(result.offset.toNumber() === dbSize.toNumber());
+
+      for(let hash of packageHashes){
+        assert(result.packageIds.includes(hash));
+      }
+    });
+
+    it('returns the correct number of packages and offset when limit is < DB size', async()=>{
+      for (let name of packageNames){
+        await packageDB.setPackage(name);
+      }
+
+      const limit = 3;
+      const firstPage = packageHashes.slice(0,limit);
+      const secondPage = packageHashes.slice(limit);
+      const dbSize = await packageDB.getNumPackages();
+
+      assert(limit < dbSize);
+
+      const resultA = await packageDB.getAllPackageIds(0,limit);
+
+      // Initial results (3)
+      assert(resultA.packageIds.length === limit);
+      assert(resultA.offset.toNumber() === limit);
+
+      const resultB = await packageDB.getAllPackageIds(resultA.offset, limit);
+
+      // Remaining results (2)
+      assert(resultB.packageIds.length === dbSize.toNumber() - limit);
+      assert(resultB.offset.toNumber() === dbSize.toNumber());
+
+      const resultC = await packageDB.getAllPackageIds(resultB.offset, limit);
+
+      // Empty results, terminal index
+      assert(resultC.packageIds.length === 0);
+      assert(resultC.offset.toNumber() === dbSize.toNumber());
+
+      // Verify that return results don't overlap
+      for (let hash in resultA.packageIds){
+        assert(!resultB.packageIds.includes(hash))
+      }
+    });
+
+    it('handles DBs with removed items correctly', async()=>{
+      for (let name of packageNames){
+        await packageDB.setPackage(name);
+      }
+
+      const limit = await packageDB.getNumPackages();
+
+      const hashToRemove = packageHashes[2];
+      await packageDB.removePackage(hashToRemove, 'because');
+
+      const resultA = await packageDB.getAllPackageIds(0,limit);
+
+      // Initial results (4)
+      assert(resultA.packageIds.length === limit - 1);
+      assert(resultA.offset.toNumber() === limit - 1);
+      assert(!resultA.packageIds.includes(hashToRemove));
+
+      const resultB = await packageDB.getAllPackageIds(resultA.offset, limit);
+
+      // Empty results, terminal index
+      assert(resultB.packageIds.length === 0);
+      assert(resultB.offset.toNumber() === resultA.offset.toNumber());
+    });
+
+    it('should be possible to extract all the package names', async()=> {
+      let names = [];
+
+      for (let name of packageNames){
+        await packageDB.setPackage(name);
+      }
+
+      const limit = await packageDB.getNumPackages();
+      const result = await packageDB.getAllPackageIds(0,limit);
+
+      assert(result.packageIds.length === packageNames.length);
+
+      for (let hash of result.packageIds){
+        let name = await packageDB.getPackageName(hash);
+        names.push(name);
+      }
+
+      for (let name of names ){
+        assert(packageNames.includes(name));
+      }
+    })
+  });
 });
