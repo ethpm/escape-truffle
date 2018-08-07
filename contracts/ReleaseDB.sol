e1ad5ab92202e89fe7735cd8439520c50dc0fe16
cgewecke <christophergewecke@gmail.com>
2018-08-07 10:37:54 -0700

    Add getAllReleaseIds method(s)

--- contracts/ReleaseDB.sol
+++ contracts/ReleaseDB.sol
@@ -231,6 +231,52 @@ contract ReleaseDB is Authorized {
     return _allReleaseHashes.get(idx);
   }
 
+  /// @dev Returns `limit` number of release hashes starting from `index` offset
+  /// @param nameHash hash of name of package being queried
+  /// @param _offset  index to begin retrieving names from. First request should use zero,
+  ///                 subsequent requests should use the returned offset value
+  /// @param limit    number of ids to return
+  function getAllReleaseHashes(bytes32 nameHash, uint _offset, uint limit)
+    public
+    view
+    returns (
+      bytes32[] releaseIds,
+      uint offset
+    )
+  {
+    bytes32[] memory hashes;                                  // Release hashes to return
+    uint cursor = _offset;                                    // Index counter to traverse DB array
+    uint remaining;                                           // Counter to collect `limit` packages
+    uint totalReleases = getNumReleasesForNameHash(nameHash); // Total number of packages in registry
+
+    // Is request within range?
+    if (cursor < totalReleases){
+
+      // Get total remaining records
+      remaining = totalReleases - cursor;
+
+      // Number of records to collect is lesser of `remaining` and `limit`
+      if (remaining > limit ){
+        remaining = limit;
+      }
+
+      // Allocate return array
+      hashes = new bytes32[](remaining);
+
+      // Collect records, skipping deleted entries
+      while(remaining > 0){
+        bytes32 hash = getReleaseHashForNameHash(nameHash, cursor);
+
+        if (releaseExists(hash)){
+          hashes[remaining - 1] = hash;
+          remaining--;
+        }
+        cursor++;
+      }
+    }
+    return (hashes, cursor);
+  }
+
   /// @dev Get the total number of releases
   /// @param nameHash the name hash to lookup.
   function getNumReleasesForNameHash(bytes32 nameHash)
