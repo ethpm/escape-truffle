1e597867e2f75032ee71d1c382cf8597ebabea40
cgewecke <christophergewecke@gmail.com>
2018-08-21 22:49:55 -0700

    Remove SemVersion deployment sim

--- test/simulations.js
+++ test/simulations.js
@@ -6,15 +6,10 @@ const helpers = require('./helpers');
 const constants = helpers.constants;
 
 const IndexedOrderedSetLib = artifacts.require('IndexedOrderedSetLib');
-const SemVersionLib = artifacts.require('SemVersionLib');
 
 contract('Gas Simulations', function(){
 
   it('IndexedOrderedSetLib deployment', async function(){
     await IndexedOrderedSetLib.new();
   });
-
-  it('SemVersionLib deployment', async function(){
-    await SemVersionLib.new();
-  })
 })
